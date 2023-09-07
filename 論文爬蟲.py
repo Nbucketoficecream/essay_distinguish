581d322251ee0b641cfe65404dc219d94b11ca11
Nbucketoficecream <113288539+Nbucketoficecream@users.noreply.github.com>
2023-09-07 21:54:28 +0800

    Update 論文爬蟲.py

--- 論文爬蟲.py
+++ 論文爬蟲.py
@@ -1,4 +1,4 @@
-﻿#coding:utf-8
+#coding:utf-8
 import requests
 from bs4 import BeautifulSoup
 from googlesearch import search
@@ -8,14 +8,27 @@ def button(url):
   }
   response=requests.get(url,headers=headers)
   soup = BeautifulSoup(response.content, 'html.parser')
-  button=soup.find_all('td')
+  button=soup.find_all('a')
   for but in button:
-   if "電子全文" in but.text:
-    if "本篇電子全文限研究生所屬學校校內系統及IP範圍內開放" in but.text:
-     return False
-    else:return True
+   if "電子全文" in but.text:   
+    return True
     break
-  
+  return False   
+def essay(url):
+  if button(url)==True:
+   headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+  }
+   response=requests.get(url,headers=headers)
+   soup = BeautifulSoup(response.content, 'html.parser')
+   essay = soup.select('font.fullhight_light')
+   for item in essay:
+            if "本篇電子全文限研究生所屬學校校內系統及IP範圍內開放" in item.text:
+                return True
+  else:return False
+   
+       
+     
 with open("(電子論文)能在論文系統上找到.txt","w",encoding='utf-8') as filec:  
  with open("(紙本論文)能在論文系統上找到.txt","w",encoding='utf-8') as filea:
   with open("無法在論文系統上找到.txt","w",encoding='utf-8') as fileb:
@@ -25,15 +38,19 @@ with open("(電子論文)能在論文系統上找到.txt","w",encoding='utf-8') 
     for name in names:
      name=name.strip( )
      num=0
-     for j in search(name, stop=5,lang="zh-tw"):
-      if "ndltd.ncl.edu.tw" in j:
-       if button(j)==True:
-        filec.write("%s:%s\n"%(name,j))
+     for url in search(name, stop=5,lang="zh-tw"):
+      if "ndltd.ncl.edu.tw" in url:
+       if button(url)==True:
+        if essay(url)==True:
+         filea.write("%s:%s\n"%(name,url))
+        else:
+         filec.write("%s:%s\n"%(name,url))
        else:
-        filea.write("%s:%s\n"%(name,j))
+        filea.write("%s:%s\n"%(name,url))
        break
       else:
        num+=1
       if num==5:
        fileb.write("%s並沒有在臺灣博碩士論文知識加值系統出現\n"%name)
 print("已完成")
+
